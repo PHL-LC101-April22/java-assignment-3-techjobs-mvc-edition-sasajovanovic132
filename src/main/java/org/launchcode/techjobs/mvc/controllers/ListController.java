c34d2033914c22272bdae59f749877db0c477ab3
sasajovanovic132 <75175528+sasajovanovic132@users.noreply.github.com>
2022-09-13 00:50:23 -0400

    Task one completed

--- src/main/java/org/launchcode/techjobs/mvc/controllers/ListController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/ListController.java
@@ -28,7 +28,7 @@ public class ListController {
         columnChoices.put("location", "Location");
         columnChoices.put("positionType", "Position Type");
         columnChoices.put("coreCompetency", "Skill");
-
+        tableChoices.put("all", JobData.findAllByName());
         tableChoices.put("employer", JobData.getAllEmployers());
         tableChoices.put("location", JobData.getAllLocations());
         tableChoices.put("positionType", JobData.getAllPositionTypes());
